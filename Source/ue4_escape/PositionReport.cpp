f0c043f765a8fc37ff2ea03e2775418acdb19208
Daniel Stankiewicz <traitorablepl@gmail.com>
2018-07-17 14:50:44 +0200

    Getting position report with loaction coordinates for specific object

--- Source/ue4_escape/PositionReport.cpp
+++ Source/ue4_escape/PositionReport.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PositionReport.h"
+#include "GameFramework/Actor.h"
 
 
 // Sets default values for this component's properties
@@ -18,9 +19,9 @@ UPositionReport::UPositionReport()
 void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
-
-	// ...
-	UE_LOG(LogTemp, Warning, TEXT("Position report ready for chair reporting!"));
+	FString ObjectName = GetOwner()->GetName();
+	FString ObjectPos = GetOwner()->GetActorLocation().ToString();
+	UE_LOG(LogTemp, Warning, TEXT("%s is reporting at position %s"), *ObjectName, *ObjectPos);
 }
 
 
